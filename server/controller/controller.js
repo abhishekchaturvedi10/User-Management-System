510e30f519539fe5867617d8be3bec488b9834fc
Abhishek Chaturvedi <abhishek19401@iiitd.ac.in>
2021-06-23 02:45:55 +0530

    CRUD APIs working

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,39 +1,97 @@
 var Userdb = require('../model/model');
 
 exports.create = (req, res) => {
-  
-  if(!req.body) {
-    res.status(400).send({message: "Content cannot be empty!"});
+
+  if (!req.body) {
+    res.status(400).send({ message: "Content cannot be empty!" });
     return;
   }
 
-  const user = new Userdb ({
+  const user = new Userdb({
     name: req.body.name,
-    email:req.body.email,
-    gender:req.body.gender,
-    status:req.body.status
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status
   });
 
-  user  
+  user
     .save(user)
     .then(data => {
       res.send(data)
     })
     .catch(err => {
       res.status(500).send({
-        message:err.message || "Some error occurred while creating a create operation"
+        message: err.message || "Some error occurred while creating a create operation"
       })
     })
 }
 
 exports.find = (req, res) => {
 
+  if (req.query.id) {
+
+    const id = req.query.id;
+
+    Userdb.findById(id)
+      .then(data => {
+        if (!data) {
+          res.status(404).send({ message: "Not found user with id = " + id });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch(err => {
+        res.status(500).send({ message: "Error retrieving user with id = " + id });
+      })
+
+    return;
+  }
+
+  Userdb.find()
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message || "Error occurred while retrieving user data" });
+    })
 }
 
 exports.update = (req, res) => {
 
+  if (!req.body) {
+    return res.status(400).send({ message: err.message || "Data to update cannot be empty" });
+  }
+
+  const id = req.params.id;
+
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({ message: `Cannot update user with id = ${id}` });
+      }
+      else {
+        res.send(data);
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Error in updating user data" });
+    })
 }
 
 exports.delete = (req, res) => {
 
+  const id = req.params.id;
+
+  Userdb.findByIdAndDelete(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({ message: `Cannot delete user with id = ${id}` });
+      }
+      else {
+        res.send(data);
+      }
+    })
+    .catch(err => {
+      res.status(500).send({ message: `Error in deleting user with id = ${id}` });
+    })
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -12,7 +12,7 @@ app.get('/update-user', services.update_user)
 
 app.post(`/api/users`, controller.create);
 app.get(`/api/users`, controller.find);
-app.put(`/api/users:id`, controller.update);
-app.delete(`/api/users:id`, controller.delete);
+app.put(`/api/users/:id`, controller.update);
+app.delete(`/api/users/:id`, controller.delete);
 
 module.exports = app
